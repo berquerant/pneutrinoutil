115147e9cbb2f3735e761dca2e77d247e111b7f3
berquerant <sin@sin-pro.local>
2025-02-22 20:50:00 +0900

    Move join() to Dir method

--- pkg/task/dir.go
+++ pkg/task/dir.go
@@ -31,18 +31,18 @@ func (d Dir) PWD() string         { return d.pwd }
 func (d Dir) WorkDir() string     { return d.workDir }
 func (d Dir) NeutrinoDir() string { return d.neutrinoDir }
 
-func (Dir) ResultDir() string { return join("${WORKDIR}", "result") }
+func (d Dir) ResultDir() string { return d.join("${WORKDIR}", "result") }
 
 func (Dir) ModelDir() string  { return "./model" }
 func (Dir) BinDir() string    { return "./bin" }
 func (Dir) OutputDir() string { return "./output" }
 func (Dir) ScoreDir() string  { return "./score" }
 
-func (d Dir) MusicXMLDir() string { return join(d.ScoreDir(), "musicxml") }
-func (d Dir) LabelDir() string    { return join(d.ScoreDir(), "label") }
+func (d Dir) MusicXMLDir() string { return d.join(d.ScoreDir(), "musicxml") }
+func (d Dir) LabelDir() string    { return d.join(d.ScoreDir(), "label") }
 
-func (d Dir) FullDir() string   { return join(d.LabelDir(), "full") }
-func (d Dir) MonoDir() string   { return join(d.LabelDir(), "mono") }
-func (d Dir) TimingDir() string { return join(d.LabelDir(), "timing") }
+func (d Dir) FullDir() string   { return d.join(d.LabelDir(), "full") }
+func (d Dir) MonoDir() string   { return d.join(d.LabelDir(), "mono") }
+func (d Dir) TimingDir() string { return d.join(d.LabelDir(), "timing") }
 
-func join(elem ...string) string { return filepath.Join(elem...) }
+func (Dir) join(elem ...string) string { return filepath.Join(elem...) }
