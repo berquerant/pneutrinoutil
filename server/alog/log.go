3bbefcaf22173bf7c8b7f7c9370063f9638a27ed
berquerant <sin@sin-pro.local>
2025-03-01 04:25:58 +0900

    Init alog instance with nop logger

--- server/alog/log.go
+++ server/alog/log.go
@@ -9,11 +9,25 @@ import (
 	"time"
 )
 
-func L() *slog.Logger {
+type Logger interface {
+	Debug(msg string, args ...any)
+	Info(msg string, args ...any)
+	Warn(msg string, args ...any)
+	Error(msg string, args ...any)
+}
+
+type NopLogger struct{}
+
+func (NopLogger) Debug(_ string, _ ...any) {}
+func (NopLogger) Info(_ string, _ ...any)  {}
+func (NopLogger) Warn(_ string, _ ...any)  {}
+func (NopLogger) Error(_ string, _ ...any) {}
+
+func L() Logger {
 	return instance
 }
 
-var instance *slog.Logger
+var instance Logger = &NopLogger{}
 
 func Setup(w io.Writer, level slog.Leveler) {
 	handler := NewLTSVHandler(w, level)
