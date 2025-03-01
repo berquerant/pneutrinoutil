2d6667947562d58c561914b268da0f1f398394d1
berquerant <sin@sin-pro.local>
2025-03-01 19:26:54 +0900

    Move ltsv log handler to logx

--- pkg/logx/log.go
+++ pkg/logx/log.go
@@ -1,8 +1,12 @@
 package logx
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log/slog"
+	"strings"
+	"time"
 )
 
 func NewTextLogger(w io.Writer, level slog.Leveler) *slog.Logger {
@@ -10,4 +14,48 @@ func NewTextLogger(w io.Writer, level slog.Leveler) *slog.Logger {
 		Level: level,
 	})
 	return slog.New(handler)
+}
+
+func NewLTSVLogger(w io.Writer, level slog.Leveler) *slog.Logger {
+	handler := NewLTSVHandler(w, level)
+	return slog.New(handler)
+}
+
+type LTSVHandler struct {
+	w     io.Writer
+	level slog.Leveler
+}
+
+func NewLTSVHandler(w io.Writer, level slog.Leveler) *LTSVHandler {
+	return &LTSVHandler{
+		w:     w,
+		level: level,
+	}
+}
+
+func (h *LTSVHandler) Enabled(_ context.Context, level slog.Level) bool {
+	return level.Level() >= h.level.Level()
+}
+func (h *LTSVHandler) WithAttrs(_ []slog.Attr) slog.Handler { return h }
+func (h *LTSVHandler) WithGroup(_ string) slog.Handler      { return h }
+
+func (h *LTSVHandler) Handle(_ context.Context, r slog.Record) error {
+	var (
+		attrs = make([]string, r.NumAttrs()+3)
+		i     int
+		add   = func(k, v string) {
+			attrs[i] = fmt.Sprintf("%s=%s", k, v)
+			i++
+		}
+	)
+
+	add("time", r.Time.Format(time.RFC3339Nano))
+	add("level", r.Level.String())
+	add("msg", r.Message)
+	r.Attrs(func(attr slog.Attr) bool {
+		add(attr.Key, attr.Value.String())
+		return true
+	})
+	_, err := fmt.Fprintln(h.w, strings.Join(attrs, "\t"))
+	return err
 }
--- server/alog/log.go
+++ server/alog/log.go
@@ -1,12 +1,10 @@
 package alog
 
 import (
-	"context"
-	"fmt"
 	"io"
 	"log/slog"
-	"strings"
-	"time"
+
+	"github.com/berquerant/pneutrinoutil/pkg/logx"
 )
 
 type Logger interface {
@@ -30,45 +28,5 @@ func L() Logger {
 var instance Logger = &NopLogger{}
 
 func Setup(w io.Writer, level slog.Leveler) {
-	handler := NewLTSVHandler(w, level)
-	instance = slog.New(handler)
-}
-
-type LTSVHandler struct {
-	w     io.Writer
-	level slog.Leveler
-}
-
-func NewLTSVHandler(w io.Writer, level slog.Leveler) *LTSVHandler {
-	return &LTSVHandler{
-		w:     w,
-		level: level,
-	}
-}
-
-func (h *LTSVHandler) Enabled(_ context.Context, level slog.Level) bool {
-	return level.Level() >= h.level.Level()
-}
-func (h *LTSVHandler) WithAttrs(_ []slog.Attr) slog.Handler { return h }
-func (h *LTSVHandler) WithGroup(_ string) slog.Handler      { return h }
-
-func (h *LTSVHandler) Handle(_ context.Context, r slog.Record) error {
-	var (
-		attrs = make([]string, r.NumAttrs()+3)
-		i     int
-		add   = func(k, v string) {
-			attrs[i] = fmt.Sprintf("%s=%s", k, v)
-			i++
-		}
-	)
-
-	add("time", r.Time.Format(time.RFC3339Nano))
-	add("level", r.Level.String())
-	add("msg", r.Message)
-	r.Attrs(func(attr slog.Attr) bool {
-		add(attr.Key, attr.Value.String())
-		return true
-	})
-	_, err := fmt.Fprintln(h.w, strings.Join(attrs, "\t"))
-	return err
+	instance = logx.NewLTSVLogger(w, level)
 }
