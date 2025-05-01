fa6f832ba3dc28e7156867b781697219a3859566
berquerant <sin@sin-pro.local>
2025-05-02 01:20:28 +0900

    Add wait command

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -12,11 +12,29 @@ user() {
     client -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$@"
 }
 
+ping() {
+    docker compose exec -it mysql mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"
+}
+
+wait_ping() {
+    for c in $(seq 30) ; do
+        if (( c > 1 )) ; then
+            sleep 1
+        fi
+        if ping ; then
+            return
+        fi
+    done
+    return 1
+}
+
 readonly user="$1"
 shift
 case "$user" in
     "root") root "$@" ;;
     "user") user "$@" ;;
+    "ping") ping ;;
+    "wait") wait_ping ;;
     *)
         echo >&2 "USER requried: root or user"
         exit 1
--- bin/redis.sh
+++ bin/redis.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-docker compose exec redis redis-cli "$@"
+client() {
+    docker compose exec redis redis-cli "$@"
+}
+
+ping() {
+    client ping
+}
+
+wait_ping() {
+    for c in $(seq 30) ; do
+        if (( c > 1 )) ; then
+            sleep 1
+        fi
+        if ping ; then
+            return
+        fi
+    done
+    return 1
+}
+
+case "$1" in
+    "wait") wait_ping ;;
+    *) client "$@" ;;
+esac
