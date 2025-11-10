eb9813f548e92dfed655f5e8e2b5ddf3215eb931
berquerant <sin@sin-pro.local>
2025-11-11 00:11:15 +0900

    Use loaderData as arguments

--- ui/app/routes/info.tsx
+++ ui/app/routes/info.tsx
@@ -1,26 +1,34 @@
 import type { Route } from "./+types/info"
-import { useLoaderData } from "react-router";
 import { defaultApi } from '../api/env';
 import type { HandlerSuccessResponseHandlerVersionResponseData } from '../api/client'
 
-export async function loader(): Promise<HandlerSuccessResponseHandlerVersionResponseData> {
+export async function loader({}: Route.LoaderArgs): Promise<HandlerSuccessResponseHandlerVersionResponseData> {
   const r = await defaultApi.versionGet()
-  return r.data
+  return r.data.data
 }
 
-export function meta({ }: Route.MetaArgs) {
+export function meta({}: Route.MetaArgs) {
   return [
     { title: "Pneutrinoutil UI Info" },
     { name: "description", content: "Show information" },
   ]
 }
 
-export default async function Info() {
-  const d = useLoaderData<typeof loader>()
-  return <div>
-    <h1> Info </h1>
-    <h2> Server </h2>
-    <li> version: { d.data.version } </li>
-    <li> revision: { d.data.revision } </li>
+export default async function Info({
+  loaderData: { version, revision },
+}: Route.ComponentProps) {
+  return <div class="container">
+    <table class="table">
+    <tbody>
+    <tr>
+    <td>Version</td>
+    <td>{version}</td>
+    </tr>
+    <tr>
+    <td>Revision</td>
+    <td>{revision}</td>
+    </tr>
+    </tbody>
+    </table>
     </div>
 }
