73455304145eed01ac7ad9a4e86ad58576b90164
berquerant <sin@sin-pro.local>
2025-11-10 22:16:03 +0900

    Add meta tags to info

--- ui/app/routes/info.tsx
+++ ui/app/routes/info.tsx
@@ -1,5 +1,6 @@
-import { useLoaderData } from "react-router"
-import { defaultApi } from '../api/env'
+import type { Route } from "./+types/info"
+import { useLoaderData } from "react-router";
+import { defaultApi } from '../api/env';
 import type { HandlerSuccessResponseHandlerVersionResponseData } from '../api/client'
 
 export async function loader(): Promise<HandlerSuccessResponseHandlerVersionResponseData> {
@@ -7,11 +8,19 @@ export async function loader(): Promise<HandlerSuccessResponseHandlerVersionResp
   return r.data
 }
 
+export function meta({ }: Route.MetaArgs) {
+  return [
+    { title: "Pneutrinoutil UI Info" },
+    { name: "description", content: "Show information" },
+  ]
+}
+
 export default async function Info() {
   const d = useLoaderData<typeof loader>()
   return <div>
-    <h1>Info</h1>
-    <li>version: {d.data.version}</li>
-    <li>revision: {d.data.revision}</li>
+    <h1> Info </h1>
+    <h2> Server </h2>
+    <li> version: { d.data.version } </li>
+    <li> revision: { d.data.revision } </li>
     </div>
 }
