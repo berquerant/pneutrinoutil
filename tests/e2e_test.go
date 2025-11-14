00a8301fc841a6a84d18782c59a3a4b2e5827d6c
berquerant <sin@sin-pro.local>
2025-11-15 06:04:38 +0900

    Refactor e2e test

--- tests/e2e_test.go
+++ tests/e2e_test.go
@@ -19,37 +19,53 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func run(t *testing.T, executable string, args ...string) error {
-	t.Helper()
-	t.Logf("run[%s %s]", executable, args)
-	x := exec.Command(executable, args...)
-	x.Stdout = os.Stdout
-	x.Stderr = os.Stdin
-	return x.Run()
+const (
+	mockcli = "../dist/pneutrinoutil-mockcli"
+	gendata = "../dist/pneutrinoutil-gendata"
+	worker  = "../dist/pneutrinoutil-worker"
+	server  = "../dist/pneutrinoutil-server"
+
+	serverHost    = "0.0.0.0"
+	serverPort    = "9101"
+	storageBucket = "test"
+
+	eventuallyWaitForMax = time.Second * 20
+	eventuallyTick       = time.Millisecond * 300
+)
+
+func assertNil[T any](t assert.TestingT, v T) bool {
+	return assert.Nil(t, v, "%v should be nil", v)
 }
 
-func compile(t *testing.T, dst, src string) error {
-	return run(t, "go", "build", "-o", dst, src)
+func assertAndGet[T any](t assert.TestingT, url string) (T, bool) {
+	var d T
+	r, err := http.Get(url)
+	if !assertNil(t, err) {
+		return d, false
+	}
+	defer r.Body.Close()
+	if !assert.Equal(t, http.StatusOK, r.StatusCode) {
+		return d, false
+	}
+	b, err := io.ReadAll(r.Body)
+	if !assertNil(t, err) {
+		return d, false
+	}
+	var body handler.SuccessResponse[T]
+	if !assertNil(t, json.Unmarshal(b, &body)) {
+		return d, false
+	}
+	if !assert.True(t, body.OK) {
+		return d, false
+	}
+	return body.Data, true
+}
+
+func eventually(t *testing.T, condition func(c *assert.CollectT), msgAndArgs ...any) bool {
+	return assert.EventuallyWithT(t, condition, eventuallyWaitForMax, eventuallyTick, msgAndArgs...)
 }
 
 func TestE2E(t *testing.T) {
-	var (
-		mockCliBin = filepath.Join(t.TempDir(), "cli")
-		workerBin  = filepath.Join(t.TempDir(), "worker")
-		serverBin  = filepath.Join(t.TempDir(), "server")
-	)
-	if !(assert.Nil(t, compile(t, mockCliBin, "../mockcli/main.go"), "compile cli") &&
-		assert.Nil(t, compile(t, workerBin, "../worker/main.go"), "compile worker") &&
-		assert.Nil(t, compile(t, serverBin, "../server/main.go"), "compile server")) {
-		return
-	}
-
-	const (
-		serverHost    = "0.0.0.0"
-		serverPort    = "9101"
-		storageBucket = "test"
-	)
-
 	var (
 		redisDB  = os.Getenv("REDIS_TEST_DB")
 		redisDSN = fmt.Sprintf("redis://%s:%s/%s",
@@ -65,31 +81,27 @@ func TestE2E(t *testing.T) {
 		workerWorkDir = filepath.Join(t.TempDir(), "worker_workspace")
 	)
 
-	if !assert.Nil(t, run(t, "../bin/redis.sh", "-n", redisDB, "flushdb"), "flush redis") {
-		return
-	}
-
 	t.Log("start worker")
 	workerProcess := exec.Command(
-		workerBin,
+		worker,
 		"--redisDSN", redisDSN,
 		"--mysqlDSN", mysqlDSN,
 		"--storageS3",
 		"--storageBucket", storageBucket,
-		"--pneutrinoutil", mockCliBin,
+		"--pneutrinoutil", mockcli,
 		"--workDir", workerWorkDir,
 		"--debug",
 	)
 	workerProcess.Stdout = os.Stdout
 	workerProcess.Stderr = os.Stderr
 
-	if !assert.Nil(t, workerProcess.Start()) {
+	if !assertNil(t, workerProcess.Start()) {
 		return
 	}
 
 	t.Log("start server")
 	serverProcess := exec.Command(
-		serverBin,
+		server,
 		"--redisDSN", redisDSN,
 		"--mysqlDSN", mysqlDSN,
 		"--storageS3",
@@ -100,7 +112,7 @@ func TestE2E(t *testing.T) {
 	)
 	serverProcess.Stdout = os.Stdout
 	serverProcess.Stderr = os.Stderr
-	if !assert.Nil(t, serverProcess.Start()) {
+	if !assertNil(t, serverProcess.Start()) {
 		return
 	}
 
@@ -108,39 +120,30 @@ func TestE2E(t *testing.T) {
 		return fmt.Sprintf("http://%s:%s/v1%s", serverHost, serverPort, path)
 	}
 
-	t.Run("health", func(t *testing.T) {
-		for i := range 10 {
-			t.Logf("health check[%d]", i)
-			if i > 0 {
-				time.Sleep(300 * time.Millisecond)
-			}
-			r, err := http.Get(newUrl("/health"))
-			if err != nil {
-				continue
-			}
-			r.Body.Close()
-			if r.StatusCode == http.StatusOK {
-				return
-			}
+	eventually(t, func(c *assert.CollectT) {
+		r, err := http.Get(newUrl("/health"))
+		if !assertNil(c, err) {
+			return
 		}
-		assert.Fail(t, "healthcheck")
-	})
+		defer r.Body.Close()
+		assert.Equal(t, http.StatusOK, r.StatusCode)
+	}, "healthcheck")
 
 	t.Run("version", func(t *testing.T) {
 		r, err := http.Get(newUrl("/version"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
-		r.Body.Close()
+		defer r.Body.Close()
 		assert.Equal(t, http.StatusOK, r.StatusCode)
 	})
 
 	t.Run("debug", func(t *testing.T) {
 		r, err := http.Get(newUrl("/debug"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
-		r.Body.Close()
+		defer r.Body.Close()
 		assert.Equal(t, http.StatusOK, r.StatusCode)
 	})
 
@@ -155,22 +158,22 @@ func TestE2E(t *testing.T) {
 		var body bytes.Buffer
 		w := multipart.NewWriter(&body)
 		fw, err := w.CreateFormFile("score", scoreFileName)
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
-		if _, err := fmt.Fprintf(fw, scoreContent); !assert.Nil(t, err) {
+		if _, err := fmt.Fprintf(fw, scoreContent); !assertNil(t, err) {
 			return
 		}
-		if !assert.Nil(t, w.Close()) {
+		if !assertNil(t, w.Close()) {
 			return
 		}
 		req, err := http.NewRequest(http.MethodPost, newUrl("/proc"), &body)
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		req.Header.Set("content-type", w.FormDataContentType())
 		resp, err := http.DefaultClient.Do(req)
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		_ = resp.Body.Close()
@@ -182,69 +185,13 @@ func TestE2E(t *testing.T) {
 		newRid = rid
 	})
 
-	t.Run("wait process completed", func(t *testing.T) {
-		for i := range 20 {
-			if i > 0 {
-				time.Sleep(300 * time.Millisecond)
-			}
-			got, ok := assertAndGet[handler.GetDetailResponseData](t, newUrl("/proc/"+newRid+"/detail"))
-			if !ok {
-				continue
-			}
-			if got.Status != "succeed" {
-				continue
-			}
-			assert.Equal(t, newRid, got.RequestID)
-			assert.Equal(t, basename, got.Basename)
-			return
-		}
-	})
-
-	t.Run("process added", func(t *testing.T) {
-		data, ok := assertAndGet[handler.ListProcessResponseData](t, newUrl("/proc"))
-		if !assert.True(t, ok) {
-			return
-		}
-		if !assert.Equal(t, 1, data.Len()) {
-			return
-		}
-		got := data[0]
-		assert.Equal(t, newRid, got.RequestID)
-	})
-
-	t.Run("find process by title", func(t *testing.T) {
-		for _, tc := range []struct {
-			title  string
-			prefix string
-			hit    int
-		}{
-			{
-				title:  "not found",
-				prefix: "a" + basename,
-				hit:    0,
-			},
-			{
-				title:  "hit",
-				prefix: basename,
-				hit:    1,
-			},
-			{
-				title:  "hit by prefix",
-				prefix: basename[:4],
-				hit:    1,
-			},
-		} {
-			t.Run(tc.title, func(t *testing.T) {
-				data, ok := assertAndGet[handler.ListProcessResponseData](t, newUrl("/proc/title?prefix="+tc.prefix))
-				if !assert.True(t, ok) {
-					return
-				}
-				if !assert.Equal(t, tc.hit, data.Len()) {
-					return
-				}
-			})
-		}
-	})
+	eventually(t, func(c *assert.CollectT) {
+		got, ok := assertAndGet[handler.GetDetailResponseData](c, newUrl("/proc/"+newRid+"/detail"))
+		assert.True(c, ok)
+		assert.Equal(c, "succeed", got.Status)
+		assert.Equal(c, newRid, got.RequestID)
+		assert.Equal(c, basename, got.Basename)
+	}, "wait process completed")
 
 	t.Run("download config", func(t *testing.T) {
 		got, ok := assertAndGet[ctl.Config](t, newUrl("/proc/"+newRid+"/config"))
@@ -258,7 +205,7 @@ func TestE2E(t *testing.T) {
 
 	t.Run("download log", func(t *testing.T) {
 		r, err := http.Get(newUrl("/proc/" + newRid + "/log"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		r.Body.Close()
@@ -267,13 +214,13 @@ func TestE2E(t *testing.T) {
 
 	t.Run("download musicxml", func(t *testing.T) {
 		r, err := http.Get(newUrl("/proc/" + newRid + "/musicxml"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		defer r.Body.Close()
 		assert.Equal(t, http.StatusOK, r.StatusCode)
 		body, err := io.ReadAll(r.Body)
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		assert.Equal(t, scoreContent, string(body))
@@ -281,7 +228,7 @@ func TestE2E(t *testing.T) {
 
 	t.Run("download wav", func(t *testing.T) {
 		r, err := http.Get(newUrl("/proc/" + newRid + "/wav"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		r.Body.Close()
@@ -290,7 +237,7 @@ func TestE2E(t *testing.T) {
 
 	t.Run("download world wav", func(t *testing.T) {
 		r, err := http.Get(newUrl("/proc/" + newRid + "/world_wav"))
-		if !assert.Nil(t, err) {
+		if !assertNil(t, err) {
 			return
 		}
 		r.Body.Close()
@@ -298,35 +245,10 @@ func TestE2E(t *testing.T) {
 	})
 
 	t.Log("cancel")
-	assert.Nil(t, workerProcess.Process.Signal(syscall.SIGTERM))
-	assert.Nil(t, serverProcess.Process.Signal(syscall.SIGTERM))
+	assertNil(t, workerProcess.Process.Signal(syscall.SIGTERM))
+	assertNil(t, serverProcess.Process.Signal(syscall.SIGTERM))
 
 	t.Log("close")
-	assert.Nil(t, workerProcess.Wait())
-	assert.Nil(t, serverProcess.Wait())
-}
-
-func assertAndGet[T any](t *testing.T, url string) (T, bool) {
-	t.Helper()
-	var d T
-	r, err := http.Get(url)
-	if !assert.Nil(t, err) {
-		return d, false
-	}
-	defer r.Body.Close()
-	if !assert.Equal(t, http.StatusOK, r.StatusCode) {
-		return d, false
-	}
-	b, err := io.ReadAll(r.Body)
-	if !assert.Nil(t, err) {
-		return d, false
-	}
-	var body handler.SuccessResponse[T]
-	if !assert.Nil(t, json.Unmarshal(b, &body)) {
-		return d, false
-	}
-	if !assert.True(t, body.OK) {
-		return d, false
-	}
-	return body.Data, true
+	assertNil(t, workerProcess.Wait())
+	assertNil(t, serverProcess.Wait())
 }
