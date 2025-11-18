943e223e1540d4da1f70970bf39efae239fe0664
berquerant <sin@sin-pro.local>
2025-11-19 00:22:41 +0900

    Extract CodeModal

---
+++ ui/app/common/modal.tsx
@@ -0,0 +1,43 @@
+import { useCallback, useState } from 'react'
+import Modal from 'react-modal'
+import { CodeBlock } from './code'
+
+export default function CodeModal({ code }) {
+  const [modalOpen, setModalOpen] = useState(false)
+  const openModal = () => setModalOpen(true)
+  const closeModal = () => setModalOpen(false)
+
+  const [isCopied, setIsCopied] = useState(false)
+  const handleCopy = useCallback(async () => {
+    try {
+      await navigator.clipboard.writeText(code)
+      setIsCopied(true)
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 3000)
+    } catch (err) {
+      console.error('Failed to copy', err);
+    }
+  }, [code])
+  const copyButtonClassName = "btn btn-primary " + (isCopied ? "btn-success" : "btn-primary")
+  const copyButtonText = isCopied ? "Copied!" : "Copy"
+  return (
+    <div className="container">
+    <button type="button" className="btn btn-primary" onClick={openModal} onRequestClose={closeModal}>
+    Config
+    </button>
+      <Modal isOpen={modalOpen}>
+      <div class="row align-items-start">
+      <div class="col d-flex gap-3">
+      <button type="button" className="btn btn-danger" onClick={closeModal}>Close</button>
+      <button type="button" className={copyButtonClassName} onClick={handleCopy}>{copyButtonText}</button>
+      </div>
+      </div>
+      <hr/>
+      <div class="row">
+      {CodeBlock({code: code})}
+      </div>
+    </Modal>
+    </div>
+  )
+}
--- ui/app/detail/config.tsx
+++ ui/app/detail/config.tsx
@@ -1,45 +1,7 @@
 import type { Route } from "./+types/detail"
-import { CodeBlock } from '../common/code'
-import { useCallback, useState } from 'react'
-import Modal from 'react-modal'
+import CodeModal from '../common/modal'
 
 export default function Config(loaderData: Route.ComponentProps) {
-  const [modalOpen, setModalOpen] = useState(false)
-  const openModal = () => setModalOpen(true)
-  const closeModal = () => setModalOpen(false)
   const data = JSON.stringify(loaderData, null, "  ")
-
-  const [isCopied, setIsCopied] = useState(false)
-  const handleCopy = useCallback(async () => {
-    try {
-      await navigator.clipboard.writeText(data)
-      setIsCopied(true)
-      setTimeout(() => {
-        setIsCopied(false);
-      }, 3000)
-    } catch (err) {
-      console.error('Failed to copy', err);
-    }
-  }, [data])
-  const copyButtonClassName = "btn btn-primary " + (isCopied ? "btn-success" : "btn-primary")
-  const copyButtonText = isCopied ? "Copied!" : "Copy"
-  return (
-    <div className="container">
-    <button type="button" className="btn btn-primary" onClick={openModal} onRequestClose={closeModal}>
-    Config
-    </button>
-      <Modal isOpen={modalOpen}>
-      <div class="row align-items-start">
-      <div class="col d-flex gap-3">
-      <button type="button" className="btn btn-danger" onClick={closeModal}>Close</button>
-      <button type="button" className={copyButtonClassName} onClick={handleCopy}>{copyButtonText}</button>
-      </div>
-      </div>
-      <hr/>
-      <div class="row">
-      {CodeBlock({code: data})}
-      </div>
-    </Modal>
-    </div>
-  )
+  return CodeModal({ code: data })
 }
