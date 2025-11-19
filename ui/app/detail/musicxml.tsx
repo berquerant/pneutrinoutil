24880adef7e01c01a490e89c76f42dfe7610a510
berquerant <sin@sin-pro.local>
2025-11-20 08:29:06 +0900

    Extarct download

--- ui/app/detail/musicxml.tsx
+++ ui/app/detail/musicxml.tsx
@@ -1,13 +1,10 @@
 import type { Route } from "./+type/detail"
+import Download from '../common/download'
 
 export default function MusicXML({
   apiServerUri,
   rid,
 }: Route.ComponentProps) {
   const url = `${apiServerUri}/proc/${rid}/musicxml`
-  return (
-    <a href={url} download className="btn btn-primary">
-    MusicXML
-    </a>
-  )
+  return Download({url: url, name: "MusicXML"})
 }
