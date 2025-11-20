9f2f0fe6d5d08570974c76d0fccc0517d1c0c2cb
berquerant <sin@sin-pro.local>
2025-11-20 09:17:52 +0900

    Change name of log button

--- ui/app/detail/log.tsx
+++ ui/app/detail/log.tsx
@@ -2,5 +2,5 @@ import type { Route } from "./+types/detail"
 import CodeModal from '../common/modal'
 
 export default function Log(loaderData: Route.ComponentProps) {
-  return CodeModal({ name: "Log", code: loaderData })
+  return CodeModal({ name: "Show Log", code: loaderData })
 }
